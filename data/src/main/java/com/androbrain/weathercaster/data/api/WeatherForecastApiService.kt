92d36fc200923e59555bf750b123747575310e8a
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-02-21 23:23:13 +0100

    Limit key to ApiService

--- data/src/main/java/com/androbrain/weathercaster/data/api/WeatherForecastApiService.kt
+++ data/src/main/java/com/androbrain/weathercaster/data/api/WeatherForecastApiService.kt
@@ -9,12 +9,15 @@ private const val FORECAST_PATH = "forecast"
 
 private const val LATITUDE_QUERY = "lat"
 private const val LONGITUDE_QUERY = "lon"
+private const val APP_ID_QUERY = "appid"
+
+private const val API_KEY = "4aff0d93fc6fb6fd2fd195632dc9bbc1"
 
 interface WeatherForecastApiService {
     @GET("$FORECAST_PATH/daily")
     suspend fun getDailyForecast(
         @Query(LATITUDE_QUERY) latitude: Double,
         @Query(LONGITUDE_QUERY) longitude: Double,
-        @Query("appid") appid: String,
+        @Query(APP_ID_QUERY) appId: String = API_KEY,
     ): Response<GetForecastResponse>
 }
